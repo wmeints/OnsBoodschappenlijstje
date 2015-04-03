6f7650256ed0bc5265e7a4baa70f6cc40ff616cc
Willem Meints <willem@mein-design.nl>
2015-04-03 13:30:49 +0200

    Fix backend JSON serialization

--- backend/src/main/java/boodschappenlijstje/Application.java
+++ backend/src/main/java/boodschappenlijstje/Application.java
@@ -2,6 +2,7 @@ package boodschappenlijstje;
 
 import boodschappenlijstje.endpoints.BoodschappenResource;
 import boodschappenlijstje.endpoints.VersionResource;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +11,9 @@ public class Application extends ResourceConfig {
     public Application() {
         register(VersionResource.class);
         register(BoodschappenResource.class);
+
+        // Register the Jackson JSON provider.
+        // This is required, so that we can get JSON in/out of the REST service.
+        register(JacksonJsonProvider.class);
     }
 }
