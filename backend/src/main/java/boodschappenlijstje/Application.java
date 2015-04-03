ad1f7434abe703a5a6d1eb65eac085cac44f59d8
Willem Meints <willem@mein-design.nl>
2015-04-03 19:32:22 +0200

    Fix migration problems with the database

--- backend/src/main/java/boodschappenlijstje/Application.java
+++ backend/src/main/java/boodschappenlijstje/Application.java
@@ -3,11 +3,22 @@ package boodschappenlijstje;
 import boodschappenlijstje.endpoints.BoodschappenResource;
 import boodschappenlijstje.endpoints.VersionResource;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.flywaydb.core.Flyway;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Application extends ResourceConfig {
+    @Autowired
+    Flyway migrator;
+
+    Logger logger = LoggerFactory.getLogger(Application.class);
+    
     public Application() {
         register(VersionResource.class);
         register(BoodschappenResource.class);
