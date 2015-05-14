9299683f06a8b17e23ed0f245499793db386844d
Willem Meints <willem@mein-design.nl>
2015-05-14 19:16:36 +0200

    Add clientside controller for logon page

---
+++ src/main/resources/static/app/scripts/controller/logon.js
@@ -0,0 +1,9 @@
+(function(angular) {
+    function LogonController() {
+
+    }
+
+    LogonController.$inject = [];
+
+    angular.module('boodschappenlijstje').controller('LogonCtrl',LogonController);
+})(angular);
