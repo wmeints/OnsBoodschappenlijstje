f43002d0cfd5cf0951edf19b376c52e22b16bb05
Willem Meints <willem@mein-design.nl>
2015-05-14 15:41:39 +0200

    Clear out old sources

--- backend/src/main/resources/db/migration/V1__initial.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `boodschappendb`.`lijst` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `done` TINYINT(1) NULL,
-  `item` TEXT NULL,
-  `winkel` TEXT NULL,
-  PRIMARY KEY (`id`))
--- website/src/main/webapp/app/scripts/app.js
+++
@@ -1,9 +0,0 @@
-(function(angular) {
-    var app = angular.module('boodschappenlijstje',['ngMaterial', 'ui']);
-
-    app.config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('pink')
-            .accentPalette('orange');
-    });
-})(angular);
