0bcd94386df8e6fa3bbfb9b9f63cdb3061d4ceba
Willem Meints <willem@mein-design.nl>
2015-05-14 17:16:35 +0200

    Remove target folder

---
+++ src/main/java/boodschappenlijst/ApplicationSecurity.java
@@ -0,0 +1,29 @@
+package boodschappenlijst;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated().and()
+                .formLogin().loginPage("/account/logon").failureUrl("/account/logon?error").permitAll();
+    }
+
+    public void configure(AuthenticationManagerBuilder authentication) throws Exception {
+        authentication.jdbcAuthentication().dataSource(dataSource)
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+}
