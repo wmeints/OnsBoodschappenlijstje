eaecddcefe570a728a624652f3fad805ecc1dd94
Willem Meints <willem@mein-design.nl>
2015-05-14 15:45:05 +0200

    Add new sources

---
+++ src/test/java/boodschappenlijst/BoodschappenlijstApplicationTests.java
@@ -0,0 +1,18 @@
+package boodschappenlijst;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BoodschappenlijstApplication.class)
+@WebAppConfiguration
+public class BoodschappenlijstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
